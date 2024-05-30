af7e456ee0c715dac7eafa2353d9ce0cdde447ba
sayan404 <sayanmajumder0002@gmail.com>
2024-05-30 15:13:54 +0530

    all the important functionalitiees are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [inputText, setInputText] = useState("")
+  const [responseText, setResponseText] = useState("")
+  const [loading, setLoading] = useState(false)
+  // console.log(inputText);
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyB4DolawuSc0QEhkiKTh3XVPOkRu3RrIhw`
+    try {
+      setLoading(true)
+      const response = await axios.post(url, {
+        "contents": [{ "parts": [{ "text": inputText }] }],
+      })
+      //  console.log(response.data.candidates[0].content.parts[0].text);
+      setResponseText(response.data.candidates[0].content.parts[0].text)
+      setLoading(false)
+    } catch (error) {
+      console.log(error.response.data.error.message);
+      setLoading(false)
+      setResponseText(error.response.data.error.message)
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='parent-component'>
+        <div className='header-component'>Chat App Using Gemini API</div>
+        <form onSubmit={handleSubmit}>
+          <textarea value={inputText} onChange={(e) => setInputText(e.target.value)} className='chat-box' placeholder='Ask anything here...'></textarea>
+          <button type='submit'>Submit</button>
+        </form>
+        {
+          loading ? <div>
+            Loading...
+          </div> : <pre>{responseText}</pre>
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
